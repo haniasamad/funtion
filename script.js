5c5ccfda7a57eab50b80f70ceee71589a51458ee
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-10-19 19:09:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function createFruit(){
+    let fruit = "apple"
+    let price = 50
+    let desc = "good"
+    return fruit + " " + price + " " + desc;
+    
+}
+let fruit= createFruit()
+console.log(fruit);
+
